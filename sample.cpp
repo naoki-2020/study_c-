38622601a91f2178a94fd098a84612d9a88fda71
naoki-2020 <abe.naoki.0314@gmail.com>
2021-04-22 17:06:53 +0900

    first commit

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;
+    a=3;
+    b= a+3;
+    c= b+4;
+    d= d+1;
+    cout << a << b << c << endl;
+    cout << "Hello World" << endl;
+}
