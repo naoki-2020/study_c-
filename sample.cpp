8cace581a4b5ea452c7e2f8a5272a49ccd25bae6
naoki-2020 <abe.naoki.0314@gmail.com>
2021-04-22 17:34:54 +0900

    second

--- sample.cpp
+++ sample.cpp
@@ -4,11 +4,10 @@ using namespace std;
 
 int main()
 {
-    int a,b,c,d;
+    int a,b,c;
     a=3;
     b= a+3;
     c= b+4;
-    d= d+1;
     cout << a << b << c << endl;
     cout << "Hello World" << endl;
 }
